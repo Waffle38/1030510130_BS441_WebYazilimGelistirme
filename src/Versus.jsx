0c7d693b1c7b00f2f46fb96837f2706e8aab961b
Waffle38 <57353355+Waffle38@users.noreply.github.com>
2022-12-18 21:16:28 +0300

    added states to Versus.jsx and added VersusGame component where the selection of the player and the AI will be shown.

--- src/Versus.jsx
+++ src/Versus.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import {VersusGame} from "./VersusGame";
 
 export function Versus()
 {
+    // selection 0 = empty, 1 = rock, 2 = paper, 3 = scissor
+    const [selection , setSelection] = useState(0);
+    const [selectionAI , setSelectionAI] = useState(0);
+
+    const rockPath = "images/Rock.png";
+    const paperPath = "images/Paper.png";
+    const scissorPath = "images/Scissors.png";
+
     return (
         <div className="versus">
             <h1>Versus</h1>
             <p>Choose a button</p>
             <div className="buttons">
-                <button><img src="images/Rock.png" alt="Rock"/></button>
-                <button><img src="images/Paper.png" alt="Papers"/></button>
-                <button><img src="images/Scissors.png" alt="Scissors"/></button>
+                <button><img src={rockPath} alt="Rock"/></button>
+                <button><img src={paperPath} alt="Papers"/></button>
+                <button><img src={scissorPath} alt="Scissors"/></button>
             </div>
+            { selection && <VersusGame sources={[rockPath, paperPath, scissorPath]} sel={selection} selAI={selectionAI}/>}
         </div>
     );
 }
